6aa18b4fe49d55caa8f32594855ab3f919025b95
zxg <zxg@shichazhe.com>
2016-12-17 21:31:21 +0800

    nt: add bakefile
    Added bakefile for NT.
    
    Signed-off-by: zxg <zxg@shichazhe.com>

--- include/libyuv/basic_types.h
+++ include/libyuv/basic_types.h
@@ -13,11 +13,11 @@
 
 #include <stddef.h>  // for NULL, size_t
 
-#if defined(_MSC_VER) && (_MSC_VER < 1600)
-#include <sys/types.h>  // for uintptr_t on x86
-#else
+//#if defined(_MSC_VER) && (_MSC_VER < 1600)
+//#include <sys/types.h>  // for uintptr_t on x86
+//#else
 #include <stdint.h>  // for uintptr_t
-#endif
+//#endif
 
 #ifndef GG_LONGLONG
 #ifndef INT_TYPES_DEFINED
